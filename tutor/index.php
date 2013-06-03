4fb5cb03c965ef73f4d7177eb719bfaf6d7e440c
Jeremy Kahn <jeremyckahn@gmail.com>
2013-06-02 18:39:24 -0700

    Updates /tutor ad.

--- tutor/index.php
+++ tutor/index.php
@@ -5,9 +5,9 @@
 <h1 class="center">Free tutoring</h1>
 <h2 class="center">For HTML, CSS and JavaScript</h2>
 <div class="copy-container center">
-  <p>In an effort to help move the web forward and train the next generation of web developers, I am offering free tutoring sessions via <a href="http://www.google.com/+/learnmore/hangouts/onair.html" target="_blank">Google Hangouts</a>.  These sessions <a href="http://www.youtube.com/playlist?list=PLZ79_svgJXRf-PjGM2GUCCc158yOb5ku1" target="_blank"><em>will be recorded and shared publicly via YouTube</em></a>, so that everyone may watch and learn from them.</p>
+  <p>In an effort to help move the web forward and train the next generation of web developers, I am offering free tutoring sessions via <a href="http://www.google.com/+/learnmore/hangouts/onair.html" target="_blank">Google Hangouts</a>.  These sessions will be recorded and shared publicly via YouTube so that everyone may watch and learn from them.  You can watch previous tutoring sessions <a href="http://www.youtube.com/playlist?list=PLZ79_svgJXRf-PjGM2GUCCc158yOb5ku1" target="_blank">here</a>.</p>
   <h3>Who am I?</h3>
-  <p>I'm Jeremy.  I'm a Web Developer at YouTube, have <a href="https://github.com/jeremyckahn/" target="_blank">written numerous JavaScript libraries</a>, am <a href="https://gist.github.com/paulmillr/2657075" target="_blank">among the most active users on Github</a> and have given talks at <a href="http://www.youtube.com/watch?v=OGZB7lq9-Mk" target="_blank">conferences</a> and <a href="http://www.youtube.com/watch?v=DVY1YMOhWn8" target="_blank">meetups</a>.</p>
+  <p>I'm Jeremy.  I'm a Web Developer at YouTube, have <a href="https://github.com/jeremyckahn/" target="_blank">written numerous JavaScript libraries</a>, and have given talks at <a href="http://www.youtube.com/watch?v=OGZB7lq9-Mk" target="_blank">conferences</a> and <a href="http://www.youtube.com/watch?v=DVY1YMOhWn8" target="_blank">meetups</a>.</p>
   <h3>How does it work?</h3>
   <p>These sessions will be completely free-form.  There is no curriculum, simply bring your questions and code problems, and I will do my best to help you figure them out.  I can't guarantee that I will always know the answer, but I'm pretty good at solving problems.  At the very least, I will get you closer to a solution.  Examples of what I can help you with in a tutoring session:</p>
   <ul>
@@ -17,19 +17,9 @@
     <li>Structuring an application or website</li>
     <li>General Q&amp;A about web development</li>
   </ul>
-  <p>Of course, I will do my best to work with you on whatever front-end challenges you have.  My goal is to help you, and by recording these sessions, help others with similar problems.  If you'd like to have a free tutoring session, please keep the following considerations in mind:</p>
-  <ul>
-    <li>I will ask you to share your screen.  These videos will be broadcast and recorded for public consumption, so please consider what is on your screen during a session.</li>
-    <li>Any code you need help with should be shareable  - something that you don't mind others seeing and copying.</li>
-    <li>Please have an up-to-date version of <a href="https://www.google.com/intl/en/chrome/browser/" target="_blank">Google Chrome</a> installed.  Although I'm happy to help debug cross-browser compatibility issues, using Chrome as a baseline will ensure that all web-based development tools we'll use will work reliably.</li>
-    <li>I may ask you to share code via Github.  So, please have a Github account set up.</li>
-    <li>You will need a Google account to use Hangouts On Air.</li>
-    <li>You will need a working webcam and microphone.</li>
-    <li>I own the rights to the videos being recorded.</li>
-  </ul>
-  <p>The above points apply to me as well - I will share my screen and code as needed.</p>
+  <p>Of course, I will do my best to work with you on whatever front-end challenges you have.  My goal is to help spread knowledge of how to build better websites and applications.</p>
   <h3>Interested?</h3>
-  <p>If you would like a tutoring session, please <a href="mailto:jeremyckahn@gmail.com?subject=Hi!%20Please%20tutor%20me!">email me</a> with the subject "Hi!  Please tutor me!" and we will schedule a session.  I'd like to limit the sessions to about a half hour, but I am flexible and will not cut you off abruptly.  Repeat sessions are welcomed.  I am generally available after normal work hours and on weekends (PDT/UTC-7 timezone, you can use <a href="http://everytimezone.com/" target="_blank">everytimezone.com</a> to find your time difference).</p>
+  <p>If you would like a tutoring session, please <a href="mailto:jeremyckahn@gmail.com?subject=Hi!%20Please%20tutor%20me!">email me</a> with the subject "Hi!  Please tutor me!" and we will schedule a session.  I'd like to limit the sessions to about a half hour, but I am flexible and will not cut you off abruptly.  Repeat sessions are welcomed.  I am generally available on weekends.  I am based in San Francisco, which is in the PDT/UTC-7 timezone (you can use <a href="http://everytimezone.com/" target="_blank">everytimezone.com</a> to find your time difference).</p>
   <p>I look forward to helping you!</p>
 </div>
 <?php
