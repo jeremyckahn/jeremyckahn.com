bb6fc21bd8455335dd2dcfa124148440a4c5d0d9
Jeremy Kahn <jeremyckahn@gmail.com>
2013-08-29 22:12:06 -0700

    Re-enables tutoring ad.
    This reverts commit 18d016028db39c7614ad9c0bf8e45af9dfee72b4.

--- tutor/index.php
+++ tutor/index.php
@@ -5,8 +5,6 @@
 <h1 class="center">Free tutoring</h1>
 <h2 class="center">For HTML, CSS and JavaScript</h2>
 <div class="copy-container center">
-  <p style="font-weight: bold;">I am not currently scheduling new sessions, as I have been overwhelmed with requests as of late.  Once I am finished with the current round of tutoring sessions, I will be available for new students.  Sorry about that, but keep an eye on this page for updates!</p>
-  <del>
   <p>In an effort to help move the web forward and train the next generation of web developers, I am offering free tutoring sessions via <a href="http://www.google.com/+/learnmore/hangouts/onair.html" target="_blank">Google Hangouts</a>.  These sessions will be recorded and shared publicly via YouTube so that everyone may watch and learn from them.  You can watch previous tutoring sessions <a href="http://www.youtube.com/playlist?list=PLZ79_svgJXRf-PjGM2GUCCc158yOb5ku1" target="_blank">here</a>.</p>
   <h3>Who am I?</h3>
   <p>I'm Jeremy.  I'm a Web Developer at YouTube, have <a href="https://github.com/jeremyckahn/" target="_blank">written numerous JavaScript libraries</a>, and have given talks at <a href="http://www.youtube.com/watch?v=OGZB7lq9-Mk" target="_blank">conferences</a> and <a href="http://www.youtube.com/watch?v=DVY1YMOhWn8" target="_blank">meetups</a>.</p>
@@ -23,7 +21,6 @@
   <h3>Interested?</h3>
   <p>If you would like a tutoring session, please <a href="mailto:jeremyckahn@gmail.com?subject=Hi!%20Please%20tutor%20me!">email me</a> with the subject "Hi!  Please tutor me!" and we will schedule a session.  I'd like to limit the sessions to about a half hour, but I am flexible and will not cut you off abruptly.  Repeat sessions are welcomed.  I am generally available on weekends.  I am based in San Francisco, which is in the PDT/UTC-7 timezone (you can use <a href="http://everytimezone.com/" target="_blank">everytimezone.com</a> to find your time difference).</p>
   <p>I look forward to helping you!</p>
-  </del>
 </div>
 <?php
   require('../tmpl_end.php');
