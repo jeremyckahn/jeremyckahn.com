dc4e635cf17b9062e2aec9bf8dea4119a3167df7
Jeremy Kahn <jeremyckahn@gmail.com>
2013-10-02 20:54:12 -0500

    Updates current employer info.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <h1 class="center">Jeremy Kahn</h1>
 <h2 class="center">Front End Web Developer</h2>
 <div class="copy-container center">
-  <p>Hello.  I'm Jeremy, and I make web apps.  I'm motivated by building awesome products and solving hard problems.  I pride myself on writing readable code that works as well tomorrow as it does today.  I believe strongly in the value of open source software and share as much code as I can.  I work at YouTube.  My most interesting recent pet projects include <a href="http://jeremyckahn.github.com/stylie/" target="_blank">Stylie</a> and <a href="http://rekapi.com/" target="_blank">Rekapi</a>.  I also offer <a href="/tutor">free online tutoring</a> in front-end development.</p>
+  <p>Hello.  I'm Jeremy, and I make web apps.  I'm motivated by building awesome products and solving hard problems.  I pride myself on writing readable code that works as well tomorrow as it does today.  I believe strongly in the value of open source software and share as much code as I can.  I work at <a href="http://www.jellyvisionlab.com/" target="_blank">Jellyvision Lab, Inc.</a>  My most interesting recent pet projects include <a href="http://jeremyckahn.github.com/stylie/" target="_blank">Stylie</a> and <a href="http://rekapi.com/" target="_blank">Rekapi</a>.  I also offer <a href="/tutor">free online tutoring</a> in front-end development.</p>
 </div>
 
 <?php
